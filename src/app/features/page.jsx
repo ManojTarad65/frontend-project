f49bc909adfa40e7db4909157f010e3dc037e1e9
MANOJ TARAD <xmanoj180@gmail.com>
2025-05-06 05:36:40 +0530

    created features

---
+++ src/app/features/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-green-700 h-screen w-screen text-8xl text-white text-center'>features</div>
+  )
+}
+
+export default page
